98ecfdaa250e2a35217b8f46ca0d97edaf5da818
KimHyungkeun <fnfn9087@gmail.com>
2019-09-09 13:05:27 +0900

    0909

--- semester3-2/oslab/ssuos_p1/src/bootloader/bootprint.asm
+++ semester3-2/oslab/ssuos_p1/src/bootloader/bootprint.asm
@@ -20,13 +20,13 @@ mov ax,ds
 mov es,ax
 
 lea bp,[msg]
-mov ah,0x13 ;문자열 출력 함수
 mov cx,0x18 ;문자열 길이
 mov al,0x00 ;쓰기모드
 mov bh,0x00 ;페이지번호
 mov bl,0x07 ;글자속성
 mov dh, 0x00 ;x좌표 0
 mov dl, 0x00 ;y좌표 0
+mov ah,0x13 ;문자열 출력 함수
 int 0x10 ;문자열 출력
 
-msg db "Hello, Hyungkeun's World", 0
+msg db "Hello, Hyungkeun's World", 0 ;출력할 문자열
